cbe5c54871c9be798879c059446252793226b71b
agungfbrn29 <agungfbrn290801@gmail.com>
2023-10-09 20:29:00 +0700

    first commit

---
+++ components/index.ts
@@ -0,0 +1,26 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import SearcBar from './SearcBar'
+import CustomFilter from './CustomFilter'
+import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
+
+
+
+
+
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearcBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+}
